93e4c3ea2690cec56fac639ff7bc4060a658f66c
Max Tang <yllfever@gmail.com>
2015-06-04 00:05:09 +0800

    makefile usage

--- PngAlphaGenerator/GenAlpha.cpp
+++ PngAlphaGenerator/GenAlpha.cpp
@@ -16,9 +16,11 @@ int main(int argc, char**argv)
 
     int width = reader.getwidth();
     int height = reader.getheight();
-	
+
+#ifdef PRINTSRCINFO	
     std::cout << width <<"," << height << " :" << reader.getbitdepth() << std::endl;
-    
+#endif
+
     pngwriter writer(width, height, 0, argv[2]);
 
     for (int j = 0; j < height; ++j)
@@ -26,12 +28,9 @@ int main(int argc, char**argv)
         for (int i = 0; i < width; ++i)
         {
             int alpha = reader.read(i+1, j+1, 4);
-            //alpha = 256 * 255 + 255;
-            //std::cout << alpha << std::endl;
             writer.plot(i, j, alpha, alpha, alpha);
         }
     }
-    //writer.line(10, 10, 100, 100, 65535, 0, 0);
 
 	writer.write_png();
 	writer.close();
